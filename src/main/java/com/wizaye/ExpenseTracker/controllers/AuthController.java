b7073159a87f39ebcd839c0c02375ba70f0a2462
wizaye <vijayendhergatla21@gmail.com>
2025-01-26 18:06:31 +0530

    add: Controller for handling Auth requests

--- src/main/java/com/wizaye/ExpenseTracker/controllers/AuthController.java
+++ src/main/java/com/wizaye/ExpenseTracker/controllers/AuthController.java
@@ -1,4 +1,26 @@
 package com.wizaye.ExpenseTracker.controllers;
 
+import com.wizaye.ExpenseTracker.services.AuthService;
+import com.wizaye.ExpenseTracker.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestParam("username") String username,@RequestParam("email") String email, @RequestParam("password") String password) {
+        return authService.register(username,email, password);
+    }
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestParam("username") String username,@RequestParam("email") String email, @RequestParam("password") String password) {
+        return authService.login(username, email, password);
+    }
+
 }
