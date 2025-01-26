2d56b20b3eea009b4f88b34932bf90fe7e2a7428
wizaye <vijayendhergatla21@gmail.com>
2025-01-26 18:05:45 +0530

    feat: JWT Bearer Auth UI and Custom API URL

--- src/main/java/com/wizaye/ExpenseTracker/config/OpenApiConfig.java
+++ src/main/java/com/wizaye/ExpenseTracker/config/OpenApiConfig.java
@@ -1,6 +1,9 @@
 package com.wizaye.ExpenseTracker.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,17 +14,25 @@ public class OpenApiConfig {
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("public-api")
+                .group("expense-tracker-public-api")
                 .pathsToMatch("/**")
                 .build();
     }
 
     @Bean
-    public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
-        return new io.swagger.v3.oas.models.OpenAPI()
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
                 .info(new Info()
-                        .title("My API")
+                        .title("Expense Tracker")
                         .version("1.0")
-                        .description("This is the API documentation for My Application"));
+                        .description("This is the API documentation for Expense Tracker"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) // Apply security globally
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
